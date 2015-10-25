c112dbd18153b0cde2132cbdb4a472d07c692df9
Dmytro Les <dmitriy.s.les@gmail.com>
2015-10-25 13:44:13 +0200

    Release 1.0.1

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+    name: 'polyroid:font-roboto',
+    version: '1.0.1',
+    summary: 'An HTML import for Roboto',
+    git: 'git@github.com:Polyroid/font-roboto.git',
+    documentation: null
+});
+
+Package.onUse(function (api) {
+    api.versionsFrom('1.2');
+
+    api.addAssets([
+        'roboto.html'
+    ], 'client');
+});
